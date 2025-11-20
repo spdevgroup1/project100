87d128df221d39da9f8856da35485a3af3b614c4
spdevgroup1 <manager@spdevgroup.com>
2025-11-20 16:46:35 +1100

    Create myTutorials.js

---
+++ myTutorials.js
@@ -0,0 +1,26 @@
+myFunction([
+{
+"display": "HTML Tutorial",
+"url": "https://www.w3schools.com/html/default.asp"
+},
+{
+"display": "CSS Tutorial",
+"url": "https://www.w3schools.com/css/default.asp"
+},
+{
+"display": "JavaScript Tutorial",
+"url": "https://www.w3schools.com/js/default.asp"
+},
+{
+"display": "SQL Tutorial",
+"url": "https://www.w3schools.com/sql/default.asp"
+},
+{
+"display": "PHP Tutorial",
+"url": "https://www.w3schools.com/php/default.asp"
+},
+{
+"display": "XML Tutorial",
+"url": "https://www.w3schools.com/xml/default.asp"
+}
+])
